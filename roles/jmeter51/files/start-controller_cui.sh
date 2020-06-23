d2ab63c908a89994234d66e0fb90eaf3d6df591a
root <root@BYD-NPC-023.localdomain>
2020-06-24 06:00:00 +0900

    add python-script

--- roles/jmeter51/files/start-controller_cui.sh
+++ roles/jmeter51/files/start-controller_cui.sh
@@ -7,9 +7,12 @@ LOGDIR=/var/www/html/${DATE}
 #JMXファイルを指定
 FILE_JMX=/usr/local/jmeter/bin/templates/build-web-test-plan.jmx
 
-#日付ディレクトリの作成
+# 日付ディレクトリの作成
 mkdir -p ${LOGDIR}
 /usr/local/jmeter/bin/jmeter -Dsun.net.inetaddr.ttl=0 -n -t ${FILE_JMX} -j ${LOGDIR}/${OPTIME}.log -l ${LOGDIR}/${OPTIME}.jtl -e -o ${LOGDIR}/${OPTIME}_th${JMETER_THREAD}${2}/ -r
 
-#CSVファイルの作成
+# CSVファイルの作成
 cat ${LOGDIR}/${OPTIME}_th${JMETER_THREAD}${2}/statistics.json | jq  -r ". [] | [.transaction,.sampleCount,.errorCount,.errorPct,.meanResTime,.minResTime,.maxResTime,.pct1ResTime,.pct2ResTime,.pct3ResTime,.throughput,.receivedKBytesPerSec,.sentKBytesPerSec] | @csv" | grep "Total" > ${LOGDIR}/${OPTIME}_th${JMETER_THREAD}${2}/statistics.csv
+
+# スプレッドシートに結果を出力
+/usr/local/bin/main.py ${LOGDIR}/${OPTIME}_th/statistics.csv
