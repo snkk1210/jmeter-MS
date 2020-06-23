0533db124a1109f15f8fdf082e54eaf526c319b1
root <root@BYD-NPC-023.localdomain>
2020-06-24 04:35:13 +0900

    modify start script

--- roles/jmeter51/files/start-controller_cui.sh
+++ roles/jmeter51/files/start-controller_cui.sh
@@ -2,14 +2,14 @@
 
 DATE=$(date +"%Y%m%d")
 OPTIME=$(date +"%Y%m%d-%H%M%S")
-# 結果の出力先ディレクトリを指定
-LOGDIR=/var/www/html/${DATE}_tutorial
-# JMXファイルを指定
+#結果の出力先ディレクトリを指定
+LOGDIR=/var/www/html/${DATE}
+#JMXファイルを指定
 FILE_JMX=/usr/local/jmeter/bin/templates/build-web-test-plan.jmx
 
-# 日付ディレクトリの作成
+#日付ディレクトリの作成
 mkdir -p ${LOGDIR}
-/usr/local/jmeter/bin/jmeter -Dsun.net.inetaddr.ttl=0 -n -t ${FILE_JMX} -j ${LOGDIR}/${OPTIME}.log -l ${LOGDIR}/${OPTIME}.jtl -e -o ${LOGDIR}/${OPTIME}_th${JMETER_THREAD}_tutorial_node${2}/ -r
+/usr/local/jmeter/bin/jmeter -Dsun.net.inetaddr.ttl=0 -n -t ${FILE_JMX} -j ${LOGDIR}/${OPTIME}.log -l ${LOGDIR}/${OPTIME}.jtl -e -o ${LOGDIR}/${OPTIME}_th${JMETER_THREAD}${2}/ -r
 
-# CSVの結果を作成
-cat ${LOGDIR}/${OPTIME}_th${JMETER_THREAD}_tutorial_node${2}/statistics.json | jq  -r ". [] | [.transaction,.sampleCount,.errorCount,.errorPct,.meanResTime,.minResTime,.maxResTime,.pct1ResTime,.pct2ResTime,.pct3ResTime,.throughput,.receivedKBytesPerSec,.sentKBytesPerSec] | @csv" | grep "Total" > ${LOGDIR}/${OPTIME}_th${JMETER_THREAD}_tutorial_node${2}/statistics.csv
+#CSVファイルの作成
+cat ${LOGDIR}/${OPTIME}_th${JMETER_THREAD}${2}/statistics.json | jq  -r ". [] | [.transaction,.sampleCount,.errorCount,.errorPct,.meanResTime,.minResTime,.maxResTime,.pct1ResTime,.pct2ResTime,.pct3ResTime,.throughput,.receivedKBytesPerSec,.sentKBytesPerSec] | @csv" | grep "Total" > ${LOGDIR}/${OPTIME}_th${JMETER_THREAD}${2}/statistics.csv
