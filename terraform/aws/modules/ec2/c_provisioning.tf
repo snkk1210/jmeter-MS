2d5a1e2a764ee1812dc457647d49be7abbbbf8c6
snkk1210 <n41210guitar@gmail.com>
2023-05-24 03:18:33 +0900

    add: c provisioner

--- terraform/aws/modules/ec2/c_provisioning.tf
+++ terraform/aws/modules/ec2/c_provisioning.tf
@@ -7,3 +7,13 @@ resource "null_resource" "ansible_c_inventory_generator" {
     working_dir = "./modules/ec2/bin"
   }
 }
+
+resource "null_resource" "ansible_c_provisioner" {
+  count      = var.c_number
+  depends_on = [aws_instance.controller]
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ./terraform/aws/modules/ec2/bin/ansible/hosts-c-${aws_instance.controller[count.index].public_ip} target.yml --private-key=./terraform/aws/modules/ec2/secret_key/jmeter.key"
+    working_dir = "./../.."
+  }
+}
