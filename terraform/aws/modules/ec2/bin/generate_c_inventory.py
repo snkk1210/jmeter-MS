b4ddde280b027566255eaeeb901d0a7123909536
snkk1210 <n41210guitar@gmail.com>
2023-05-23 23:23:01 +0900

    modify: comment

--- terraform/aws/modules/ec2/bin/generate_c_inventory.py
+++ terraform/aws/modules/ec2/bin/generate_c_inventory.py
@@ -2,13 +2,13 @@
 
 import sys
 
-# インベントリファイルパス
+# inventory file path
 inventory_file = "ansible/hosts"
 
-# EC2 インスタンスのパブリック IP を取得
+# Get the public IP of the EC2 instance
 public_ip = sys.argv[1]
 
-# インベントリファイルに IP アドレスを追加
+# Add IP address to inventory file
 with open(inventory_file, "w") as f:
     f.write("[controller]\n")
     f.write(public_ip)
