140872b36d27a0f105a9a34641cac0609b8c657f
snkk1210 <n41210guitar@gmail.com>
2023-05-24 02:02:40 +0900

    fix: hosts gen script

--- terraform/aws/modules/ec2/c_provisioning.tf
+++ terraform/aws/modules/ec2/c_provisioning.tf
@@ -3,7 +3,7 @@ resource "null_resource" "ansible_c_provisioner" {
   depends_on = [aws_instance.controller]
 
   provisioner "local-exec" {
-    command = "python3 ansible/generate_c_inventory.py ${aws_instance.controller[count.index].public_ip}"
+    command = "python3 ./generate_c_inventory.py ${aws_instance.controller[count.index].public_ip}"
     working_dir = "./modules/ec2/bin"
   }
 }
