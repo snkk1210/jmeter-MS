6f19d2e4e186070dda65b22f4206419c7b5d4f76
snkk1210 <n41210guitar@gmail.com>
2024-06-28 22:36:04 +0900

    modify: consolidate key file directories

--- terraform/aws/modules/ec2/key_pair.tf
+++ terraform/aws/modules/ec2/key_pair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "key_pair" {
   key_name   = "${var.common.project}-${var.common.environment}-jmeter-secret-key"
-  public_key = file("${path.module}/public_key/jmeter.pub")
+  public_key = file("${path.module}/key/jmeter.key.pub")
 }
